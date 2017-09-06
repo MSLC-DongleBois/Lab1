412ad5ab7cf17c2a21a4b3256a49581b97d9426d
mkqueenan <kqueenan@smu.edu>
2017-09-06 18:14:00 -0500

    kitchen sink class created

---
+++ DongleBoisLab1/DongleBoisLab1/KitchenSink.h
@@ -0,0 +1,13 @@
+//
+//  KitchenSink.h
+//  DongleBoisLab1
+//
+//  Created by Kevin Queenan, Jr. on 9/6/17.
+//  Copyright © 2017 DongleBois. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KitchenSink : UIView
+
+@end
---
+++ DongleBoisLab1/DongleBoisLab1/KitchenSink.m
@@ -0,0 +1,21 @@
+//
+//  KitchenSink.m
+//  DongleBoisLab1
+//
+//  Created by Kevin Queenan, Jr. on 9/6/17.
+//  Copyright © 2017 DongleBois. All rights reserved.
+//
+
+#import "KitchenSink.h"
+
+@implementation KitchenSink
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
