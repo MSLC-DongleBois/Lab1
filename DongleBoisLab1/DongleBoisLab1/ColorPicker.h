f99034c1d48d71e27a9085596dd4262e1ca5d08c
Austin Chen <chenas@smu.edu>
2017-09-07 00:32:21 -0500

    "Added colorpicker class"

---
+++ DongleBoisLab1/DongleBoisLab1/ColorPicker.h
@@ -0,0 +1,13 @@
+//
+//  ColorPicker.h
+//  DongleBoisLab1
+//
+//  Created by Austin Chen on 9/7/17.
+//  Copyright © 2017 DongleBois. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ColorPicker : UIView
+
+@end
---
+++ DongleBoisLab1/DongleBoisLab1/ColorPicker.m
@@ -0,0 +1,21 @@
+//
+//  ColorPicker.m
+//  DongleBoisLab1
+//
+//  Created by Austin Chen on 9/7/17.
+//  Copyright © 2017 DongleBois. All rights reserved.
+//
+
+#import "ColorPicker.h"
+
+@implementation ColorPicker
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
