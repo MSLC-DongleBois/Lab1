d0456447ca6464c478867cc913dd16f333054923
Austin Chen <chenas@smu.edu>
2017-09-08 15:07:44 -0500

    Added stop functionality to Darude Sandstorm

--- DongleBoisLab1/DongleBoisLab1/TimerViewController.m
+++ DongleBoisLab1/DongleBoisLab1/TimerViewController.m
@@ -33,15 +33,15 @@
     
     [super viewDidLoad];
     
-    NSString *path = [NSString stringWithFormat:@"%@/Darude_Sandstorm.mp3", [[NSBundle mainBundle] resourcePath]];
-    NSURL *soundUrl = [NSURL fileURLWithPath:path];
-    
-    // Create audio player object and initialize with URL to sound
-    _audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:soundUrl error:nil];
-    
-    _audioPlayer.numberOfLoops = -1;
-    
-    [_audioPlayer play];
+//    NSString *path = [NSString stringWithFormat:@"%@/Darude_Sandstorm.mp3", [[NSBundle mainBundle] resourcePath]];
+//    NSURL *soundUrl = [NSURL fileURLWithPath:path];
+//    
+//    // Create audio player object and initialize with URL to sound
+//    _audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:soundUrl error:nil];
+//    
+//    _audioPlayer.numberOfLoops = -1;
+//    
+//    [_audioPlayer play];
     
     self.timerIvar  = [NSTimer scheduledTimerWithTimeInterval:0.05
                                                        target:self
@@ -58,16 +58,33 @@
     
 }
 
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear: animated];
+    
+    NSString *music = [[NSBundle mainBundle] pathForResource:@"Darude_Sandstorm" ofType:@"mp3"];
+    _audioPlayer=[[AVAudioPlayer alloc]initWithContentsOfURL:[NSURL fileURLWithPath:music] error:NULL];
+    _audioPlayer.numberOfLoops=-1;
+    [_audioPlayer play];
+}
 
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear: animated];
+    
+    _audioPlayer.delegate = nil;
+    [_audioPlayer stop];
+    _audioPlayer = nil;
+}
+
+/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
-    
-    [_audioPlayer stop];
 }
-
+*/
 
 @end
